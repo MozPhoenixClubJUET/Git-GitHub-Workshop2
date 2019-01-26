bc25d8a735bf93586f0d586af5f9644b76dbd7f8
Ritik Raj <33518732+chronos25@users.noreply.github.com>
2019-01-26 09:53:27 +0530

    fix me
    get system time & data

---
+++ fix_me.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+    SYSTEMTIME str_t;
+    GetSystemTime(&str_t);
+
+    printf("Y:M:D   -> %d:%d:%d \n h:m:s -> %d:%d:%d",
+    str_t->wYear,str_t->wMonth,str_t->wDay
+    ,str_t->wHour,str_t->wMinute,str_t->wSecond);
+
+	getch();
+}
