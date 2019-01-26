032ee2f7074c37df7f312559be20ee371566919d
Radhikav30 <43778996+Radhikav30@users.noreply.github.com>
2019-01-26 18:02:46 +0530

    bug fixed

--- fix me.c
+++ fix me.c
@@ -6,9 +6,9 @@ int main()
     SYSTEMTIME str_t;
     GetSystemTime(&str_t);
 
-    printf("Y:M:D   -> %d:%d:%d \n h:m:s -> %d:%d:%d",
-    str_t->wYear,str_t->wMonth,str_t->wDay
-    ,str_t->wHour,str_t->wMinute,str_t->wSecond);
+    printf("Y:M:D   . %d:%d:%d \n h:m:s . %d:%d:%d",
+    str_t.wYear,str_t.wMonth,str_t.wDay
+    ,str_t.wHour,str_t.wMinute,str_t.wSecond);
 
 	getch();
 }
