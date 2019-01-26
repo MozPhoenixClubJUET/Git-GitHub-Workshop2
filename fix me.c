97c8b039f2f0573c7230fcf360e4128255b62688
richatripathi17 <37188197+richatripathi17@users.noreply.github.com>
2019-01-26 17:55:03 +0530

    bug fixed
    error in indirection operator

--- fix me.c
+++ fix me.c
@@ -7,8 +7,8 @@ int main()
     GetSystemTime(&str_t);
 
     printf("Y:M:D   -> %d:%d:%d \n h:m:s -> %d:%d:%d",
-    str_t->wYear,str_t->wMonth,str_t->wDay
-    ,str_t->wHour,str_t->wMinute,str_t->wSecond);
+    str_t.wYear,str_t.wMonth,str_t.wDay
+    ,str_t.wHour,str_t.wMinute,str_t.wSecond);
 
 	getch();
 }
